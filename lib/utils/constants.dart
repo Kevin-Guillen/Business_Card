24c595dfe384a365a1daba04c657e352df8e7ed8
kevin-guillen <kevin.guillen@globant.com>
2021-06-03 20:32:45 -0300

    Business Card Ui development

---
+++ lib/utils/constants.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+abstract class Constants{
+  static const Color avatarShadowColor = Colors.black;
+  static const Color personNameTextColor = Colors.white;
+  static const Color contactInfoTextColor = Colors.white;
+  static const Color positionTextColor = Colors.black54;
+  static const Color phoneIconColor = Colors.green;
+  static const Color emailIconColor = Colors.redAccent;
+  static const Color cardsBackgroundColor = Colors.black12;
+  static const double personNameFontSize = 45.0;
+  static const double phoneNumberFontSize = 20.0;
+  static const double emailFontSize = 20.0;
+
+}
