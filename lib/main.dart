4a4cf5b12d00aac0f8e02ad63de6c114ddd39e68
kevin-guillen <kevin.guillen@globant.com>
2021-06-09 11:51:26 -0300

    - Removed unnused constant - Fixed identation problem - Removed unnesesary container - added missing constants - Replaced background image widget from decoration to stack

--- lib/main.dart
+++ lib/main.dart
@@ -8,123 +8,125 @@ void main() {
 
 class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext) {
+  Widget build(buildContext) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Business Card',
       home: Scaffold(
-        backgroundColor: Colors.purple.shade300,
+        backgroundColor: Colors.purple.shade200,
         body: SafeArea(
-          child: Container(
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage(
-                  'assets/images/cardbackground.png',
-                ),
+          child: Stack(
+            fit: StackFit.expand,
+            alignment: AlignmentDirectional.center,
+            children: [
+              Image.asset(
+                'assets/images/cardbackground.png',
                 fit: BoxFit.fill,
               ),
-            ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Container(
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    boxShadow: [
-                      BoxShadow(
-                        blurRadius: Constants.avatarBlurShadow,
-                        color: Colors.black,
-                        spreadRadius: 1,
-                      ),
-                    ],
-                  ),
-                  child: CircleAvatar(
-                    backgroundImage: AssetImage(
-                      'assets/images/me.jpg',
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      boxShadow: [
+                        BoxShadow(
+                          blurRadius: Constants.avatarBlurShadow,
+                          color: Colors.black,
+                          spreadRadius: 1,
+                        ),
+                      ],
                     ),
-                    radius: Constants.avatarRadius,
-                  ),
-                ),
-                SizedBox(
-                  height: Constants.avatarAndPersonNameSizeBoxHeight,
-                ),
-                Text(
-                  'Kevin Guillen',
-                  style: TextStyle(
-                    fontSize: Constants.personNameFontSize,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
-                    letterSpacing: Constants.letterSpacing,
-                  ),
-                ),
-                SizedBox(
-                  height: Constants.personNameAndPositionSizeBoxHeight,
-                ),
-                Text(
-                  'Flutter Developer Trainee',
-                  style: TextStyle(
-                    fontSize: Constants.positionFontSize,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black54,
-                    letterSpacing: Constants.letterSpacing,
-                  ),
-                ),
-                Divider(
-                  height: 60,
-                  thickness: 3,
-                  color: Colors.blueAccent.shade100,
-                  indent: 60,
-                  endIndent: 60,
-                ),
-                Card(
-                  color: Colors.black12,
-                  child: ListTile(
-                    leading: Padding(
-                      padding: EdgeInsets.only(),
-                      child: Icon(
-                        Icons.phone,
-                        color: Colors.green,
+                    child: CircleAvatar(
+                      backgroundImage: AssetImage(
+                        'assets/images/me.jpg',
                       ),
-                    ),
-                    title: Text(
-                      '+54 9 11 2494 607181',
-                      style: contactInfoTextStyle,
-                    ),
-                    subtitle: Text(
-                      "Cell Phone",
-                      style: TextStyle(
-                        color: Colors.white,
-                      ),
+                      radius: Constants.avatarRadius,
                     ),
                   ),
-                ),
-                SizedBox(
-                  height: Constants.phoneAndEmailSizeBoxHeight,
-                ),
-                Card(
-                  color: Colors.black12,
-                  child: ListTile(
-                    leading: Padding(
-                      padding: EdgeInsets.only(
-                        top: 8.0,
-                      ),
-                      child: Icon(
-                        Icons.email,
-                        color: Colors.redAccent,
-                      ),
+                  SizedBox(
+                    height: Constants.avatarAndPersonNameSizeBoxHeight,
+                  ),
+                  Text(
+                    'Kevin Guillen',
+                    style: TextStyle(
+                      fontSize: Constants.personNameFontSize,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                      letterSpacing: Constants.letterSpacing,
                     ),
-                    title: Text('kevin.guillen@globant.com',
-                        style: contactInfoTextStyle),
-                    subtitle: Text(
-                      "Email",
-                      style: TextStyle(
-                        color: Colors.white,
+                  ),
+                  SizedBox(
+                    height: Constants.personNameAndPositionSizeBoxHeight,
+                  ),
+                  Text(
+                    'Flutter Developer Trainee',
+                    style: TextStyle(
+                      fontSize: Constants.positionFontSize,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black54,
+                      letterSpacing: Constants.letterSpacing,
+                    ),
+                  ),
+                  Divider(
+                    height: Constants.dividerHeight,
+                    thickness: Constants.dividerThickness,
+                    color: Colors.blueAccent.shade100,
+                    indent: Constants.dividerIndentStart,
+                    endIndent: Constants.dividerIndentEnd,
+                  ),
+                  Card(
+                    color: Colors.black12,
+                    child: ListTile(
+                      leading: Padding(
+                        padding: EdgeInsets.only(
+                          top: Constants.iconsTopPadding,
+                        ),
+                        child: Icon(
+                          Icons.phone,
+                          color: Colors.green,
+                        ),
+                      ),
+                      title: Text(
+                        '+54 9 11 2494 607181',
+                        style: contactInfoTextStyle,
+                      ),
+                      subtitle: Text(
+                        "Cell Phone",
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
                       ),
                     ),
                   ),
-                ),
-              ],
-            ),
+                  SizedBox(
+                    height: Constants.phoneAndEmailSizeBoxHeight,
+                  ),
+                  Card(
+                    color: Colors.black12,
+                    child: ListTile(
+                      leading: Padding(
+                        padding: EdgeInsets.only(
+                          top: Constants.iconsTopPadding,
+                        ),
+                        child: Icon(
+                          Icons.email,
+                          color: Colors.redAccent,
+                        ),
+                      ),
+                      title: Text('kevin.guillen@globant.com',
+                          style: contactInfoTextStyle),
+                      subtitle: Text(
+                        "Email",
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
           ),
         ),
       ),
--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,5 +1,3 @@
-import 'package:flutter/material.dart';
-
 abstract class Constants {
   static const double personNameFontSize = 45.0;
   static const double phoneNumberFontSize = 20.0;
@@ -10,6 +8,12 @@ abstract class Constants {
   static const double avatarRadius = 100.00;
   static const double emailFontSize = 20.0;
   static const double avatarBlurShadow = 35.0;
+  static const double dividerHeight = 60.0;
+  static const double dividerThickness = 3.0;
+  static const double dividerIndentStart = 60.0;
+  static const double dividerIndentEnd = 60.0;
+  static const double iconsTopPadding = 8.0;
+
 
   // contactInfoTextStyle
   static const mailAndPhoneNumberFontSize = 20.0;
